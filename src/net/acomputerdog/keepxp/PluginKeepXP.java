eb4932674ea7a11c94e8737ed4920b04d8b16d13
chris <acomputerdog@gmail.com>
2017-01-14 13:56:09 -0500

    Make /reload safe

--- src/net/acomputerdog/keepxp/PluginKeepXP.java
+++ src/net/acomputerdog/keepxp/PluginKeepXP.java
@@ -2,6 +2,7 @@ package net.acomputerdog.keepxp;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,6 +12,11 @@ public class PluginKeepXP extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        HandlerList.unregisterAll((JavaPlugin)this);
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
